a1339274c717bda79fbcf8fd60a2e724c4b07d33
jorgnv <jorgnv@gmail.com>
2025-01-12 12:05:51 -0500

    eliminar navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import Nav from 'components/nav'
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import Nav from "components/nav";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Nav />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
