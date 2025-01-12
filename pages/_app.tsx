3f9dd8f4d14548f0fbaacf599434ca2f0ac13f0b
jorgnv <jorgnv@gmail.com>
2025-01-12 13:33:31 -0500

    agregar speedinsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import Nav from "components/nav";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SpeedInsights />
       <Component {...pageProps} />
     </>
   );
