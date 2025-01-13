7df06687d1ef4cb92e72a0673f9db2c6376298f4
jorgnv <jorgnv@gmail.com>
2025-01-13 10:41:43 -0500

    eliminar speedinsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import Nav from "components/nav";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SpeedInsights />
       <Component {...pageProps} />
     </>
   );
